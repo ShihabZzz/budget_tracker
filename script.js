0404b13b6b437b5a078cc96d1825bb58c390332a
ShihabZzz <shihab.riadn@gmail.com>
2024-04-24 06:23:56 +0600

    Init

---
+++ script.js
@@ -0,0 +1,167 @@
+// let idx = 0;
+// let amountConatainer = [];
+// let amountData = Number(document.getElementById('amount').value);
+// let amountType = amountData > 0 ? "income" : amountData < 0 ? "expense" : "zero";
+
+// let parsingConatainer = {
+//     "idx": `${idx++}`,
+//     "amountData": `${amountData}`,
+//     "amountType": `${amountType}`,
+// };
+
+// let okClick = document.getElementById('ok');
+
+// onClick.addEventListener('click', () => {
+//     amountConatainer.push(parsingConatainer);
+//     localStorage.setItem("amounts", JSON.stringify(amountConatainer));
+// });
+
+
+let idx = 0;
+let amountContainer = [];
+
+let income = 0;
+let expense = 0;
+
+let updateIncomeUI = () => {
+    let incomeValue = `Income: ${income}`;
+    document.getElementById("incomeData").innerText = incomeValue;
+}
+
+let updateExpenseUI = () => {
+    let expenseValue = `Expense: ${expense}`;
+    document.getElementById("expenseData").innerText = expenseValue;
+}
+
+let detailsUI = () => {
+    let details = document.getElementById("detailsData");
+
+    // Empty-out at every request
+    details.innerHTML = "";
+
+    for (let index = 0; index < amountContainer.length; index++) {
+        const amount = amountContainer[index].amountData;
+        const type = amountContainer[index].amountType;
+
+        // console.log(amount);
+        // console.log(type);
+
+        let div = document.createElement('div');
+        div.classList.add("px-6", "pt-6", "flex", "justify-between", "bg-gray-200")
+
+        div.innerHTML = `
+        <div class="flex space-x-2 items-center">
+            <p class="font-medium text-xl">${amount}</p>
+            <p class="font-normal text-sm">${type}</p>
+        </div>
+        <div class="flex space-x-4">
+            <a href="#" onClick="deleteItem(${index})">Delete</a>
+            <a href="#" onClick="editItem(${index})">Edit</a>
+        </div>
+   `;
+        details.appendChild(div);
+    }
+}
+
+function deleteItem(index) {
+    let spliceAmount = amountContainer[index].amountData;
+    let spliceType = amountContainer[index].amountType;
+    amountContainer.splice(index, 1);
+    (spliceType === "income" ? income -= spliceAmount : expense -= Math.abs(spliceAmount));
+    // console.log(spliceAmount);
+
+    localStorage.setItem("income_db", income);
+    localStorage.setItem("expense_db", expense);
+    localStorage.setItem("amounts_db", JSON.stringify(amountContainer));
+
+    updateIncomeUI();
+    updateExpenseUI();
+    detailsUI();
+}
+
+function editItem(index) {
+    let itemAmount = prompt("Please Insert Updated Value", amountContainer[index].amountData);
+
+    if (itemAmount !== null && !isNaN(itemAmount)) {
+        itemAmount = Number(itemAmount);
+        let itemType = itemAmount > 0 ? "income" : itemAmount < 0 ? "expense" : "zero";
+        if (itemType === "zero") {
+            alert("Zero is not acceptable!");
+        } else {
+            (amountContainer[index].type === "income" ? income -= amountContainer[index].amountData : expense -= Math.abs(amountContainer[index].amountData));
+            (itemAmount > 0 ? income += itemAmount : expense += Math.abs(itemAmount));
+            amountContainer[index].amountData = itemAmount;
+            // console.log(itemAmount);
+            amountContainer[index].amountType = itemType;
+
+            localStorage.setItem("income_db", income);
+            localStorage.setItem("expense_db", expense);
+            localStorage.setItem("amounts_db", JSON.stringify(amountContainer));
+
+            updateIncomeUI();
+            updateExpenseUI();
+            detailsUI();
+        }
+    } else {
+        alert("Insert Proper Numerical Value!");
+    }
+}
+
+// check previous data
+let preData = localStorage.getItem("amounts_db");
+
+if (preData) {
+    amountContainer = JSON.parse(preData); // parse JSON format => array of object
+
+    // console.log(amountContainer.length)
+
+    if (amountContainer.length > 0) {
+        idx = amountContainer[amountContainer.length - 1].idx + 1 // set proper idx;
+        income = Number(localStorage.getItem("income_db"));
+        expense = Number(localStorage.getItem("expense_db"));
+    }
+
+    // console.log(amountContainer);
+    // console.log(idx);
+    // console.log(income);
+} else {
+    localStorage.setItem("income_db", income);
+    localStorage.setItem("expense_db", expense);
+    localStorage.setItem("amounts_db", JSON.stringify(amountContainer));
+}
+
+// initial calling
+updateIncomeUI();
+updateExpenseUI();
+detailsUI();
+
+let okClick = document.getElementById('ok');
+
+// click action
+okClick.addEventListener('click', () => {
+    let amountData = Number(document.getElementById('amount').value);
+    let amountType = amountData > 0 ? "income" : amountData < 0 ? "expense" : "zero";
+    if (amountType === "zero") {
+        alert("Zero or Blank is not acceptable!");
+    } else {
+        (amountData > 0 ? income += amountData : expense += Math.abs(amountData));
+        let parsingContainer = {
+            "idx": idx++,
+            "amountData": amountData,
+            "amountType": amountType,
+        };
+        amountContainer.push(parsingContainer);
+
+        localStorage.setItem("amounts_db", JSON.stringify(amountContainer));
+        localStorage.setItem("income_db", income);
+        localStorage.setItem("expense_db", expense);
+
+        // update UI
+        updateIncomeUI();
+        updateExpenseUI();
+        detailsUI();
+    }
+
+});
+
+// let deleteReq = document.getElementById(detailsData).addEventListener();
